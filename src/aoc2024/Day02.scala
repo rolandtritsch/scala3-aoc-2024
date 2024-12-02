8f6afa586e8818a61378e4c1e785ca350108eb05
Roland Tritsch <roland@tritsch.email>
2024-12-02 06:33:04 +0000

    Day02: Solved.

--- src/aoc2024/Day02.scala
+++ src/aoc2024/Day02.scala
@@ -6,24 +6,62 @@ package aoc2024
 object Day02 {
   val logger = com.typesafe.scalalogging.Logger(this.getClass.getName)
 
-  /** @return the solution for part1 */
-  def part1(is: Seq[Int]): Int = {
-    require(is.nonEmpty, "is.nonEmpty")
-    logger.debug(s"${is}")
+  private def levelIsSafe(isIncreasing: Boolean, prevLevel: Int, level: Int) = {
+    val levelDiff = math.abs(prevLevel - level)
+    if (isIncreasing) {
+      prevLevel < level && levelDiff >= 1 && levelDiff <= 3
+    } else {
+      prevLevel > level && levelDiff >= 1 && levelDiff <= 3
+    }
+  }
 
-    is(0)
+  private def reportIsSafe(report: Seq[Int], dampen: Boolean): Boolean = {
+    val initialIsIncreasing = report(0) < report(1)
+    val levelDiff = math.abs(report(0) - report(1))
+    val inititialIsSafe = levelDiff >= 1 && levelDiff <= 3
+
+    val (safe, _, _, count) = report.tail
+      .foldLeft(inititialIsSafe, initialIsIncreasing, report(0), 0) {
+        (soFar, level) =>
+          {
+            logger.debug(s"${soFar}")
+            val (safe, isIncreasing, prevLevel, counter) = soFar
+            val thisIsSave = levelIsSafe(isIncreasing, prevLevel, level)
+            val thisCounter = if (thisIsSave) counter + 1 else counter
+            (safe && thisIsSave, isIncreasing, level, thisCounter)
+          }
+      }
+
+    if (dampen) count >= report.size - 2 else count == report.size - 1
+  }
+
+  private def reportsAreSafe(reports: Seq[Seq[Int]], dampen: Boolean): Int = {
+    reports.count { (report) =>
+      {
+        logger.debug(s"${report}")
+        reportIsSafe(report, dampen)
+      }
+    }
+  }
+
+  /** @return the solution for part1 */
+  def part1(reports: Seq[Seq[Int]]): Int = {
+    require(reports.nonEmpty, "reports.nonEmpty")
+    logger.debug(s"${reports}")
+
+    reportsAreSafe(reports, false)
   }
 
   /** @return the solution for part2 */
-  def part2(is: Seq[Int]): Int = {
-    require(is.nonEmpty, "is.nonEmpty")
-    logger.debug(s"${is}")
+  def part2(reports: Seq[Seq[Int]]): Int = {
+    require(reports.nonEmpty, "reports.nonEmpty")
+    logger.debug(s"${reports}")
 
-    is(0)
+    reportsAreSafe(reports, true)
   }
 
-  /** @return the file for the given filename as parsed elements */ 
-  def readFile(filename: String): Seq[Int] = {
+  /** @return the file for the given filename as parsed elements */
+  def readFile(filename: String): Seq[Seq[Int]] = {
     import scala.io.Source
 
     require(filename.nonEmpty, "filename.nonEmpty")
@@ -33,7 +71,7 @@ object Day02 {
     try {
       source.getLines().toSeq.map { line =>
         logger.debug(s"${line}")
-        val parsed = line.toInt
+        val parsed = line.split("\\s+").map(_.toInt)
         logger.debug(s"${parsed}")
         parsed
       }
--- test/src/aoc2024/Day02Test.scala
+++ test/src/aoc2024/Day02Test.scala
@@ -3,18 +3,25 @@ package aoc2024
 class Day02Test extends munit.ScalaCheckSuite {
   test("Day02 - readFile") {
     val obtained = Day02.readFile("./inputs/Day02.txt")
-    assertEquals(obtained, Seq(1, 2, 3))
+    assertEquals(obtained(0), Seq(16, 17, 18, 21, 23, 24, 27, 24))
   }
 
-  val input = Seq(0)
+  val input = Seq(
+    Seq(7, 6, 4, 2, 1),
+    Seq(1, 2, 7, 8, 9),
+    Seq(9, 7, 6, 2, 1),
+    Seq(1, 3, 2, 4, 5),
+    Seq(8, 6, 4, 4, 1),
+    Seq(1, 3, 6, 7, 9),
+  )
 
   test("Day02 - part1") {
     val obtained = Day02.part1(input)
-    assertEquals(obtained, 0)
+    assertEquals(obtained, 2)
   }
 
   test("Day02 - part2") {
     val obtained = Day02.part2(input)
-    assertEquals(obtained, 0)
+    assertEquals(obtained, 4)
   }
 }
