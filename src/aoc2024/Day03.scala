e493afcaa3b2a9d2724854c5c717481fed3d7df7
Roland Tritsch <roland@tritsch.email>
2024-12-03 06:34:43 +0000

    Day03: part1: Solved.

--- src/aoc2024/Day03.scala
+++ src/aoc2024/Day03.scala
@@ -1,42 +1,61 @@
 package aoc2024
 
-/** Day03 - template
+/** Day03 - Mull It Over
   */
 
 object Day03 {
   val logger = com.typesafe.scalalogging.Logger(this.getClass.getName)
 
-  /** @return the solution for part1 */
-  def part1(is: Seq[Int]): Int = {
-    require(is.nonEmpty, "is.nonEmpty")
-    logger.debug(s"${is}")
+  abstract class Operation
+  case class Mul(op1: Int, op2: Int) extends Operation
 
-    is(0)
+  private def execute(instructions: Seq[Operation]): Int = {
+    instructions.foldLeft(0) { (acc, operation) => operation match {
+      case Mul(op1, op2) => acc + (op1 * op2)
+      case _ => throw new RuntimeException("Unexpected case")
+    }}
+  }
+
+  /** @return the solution for part1 */
+  def part1(instructions: Seq[Operation]): Int = {
+    require(instructions.nonEmpty, "instructions.nonEmpty")
+    logger.debug(s"instructions: ${instructions}")
+
+    execute(instructions)
   }
 
   /** @return the solution for part2 */
-  def part2(is: Seq[Int]): Int = {
-    require(is.nonEmpty, "is.nonEmpty")
-    logger.debug(s"${is}")
+  def part2(instructions: Seq[Operation]): Int = {
+    require(instructions.nonEmpty, "instructions.nonEmpty")
+    logger.debug(s"instructions: ${instructions}")
 
-    is(0)
+    val Mul(op1, op2) = instructions(0) : @unchecked
+    op1 * op2
   }
 
   /** @return the file for the given filename as parsed elements */ 
-  def readFile(filename: String): Seq[Int] = {
+  def readFile(filename: String): Seq[Operation] = {
     import scala.io.Source
+    import scala.util.matching
 
     require(filename.nonEmpty, "filename.nonEmpty")
     logger.debug(s"${filename}")
 
     val source = Source.fromFile(filename)
     try {
-      source.getLines().toSeq.map { line =>
-        logger.debug(s"${line}")
-        val parsed = line.toInt
-        logger.debug(s"${parsed}")
-        parsed
-      }
+      source.getLines().toSeq.map { line => {
+        logger.debug(s"line: ${line}")
+
+        val parseLine: matching.Regex = """mul\(\d{1,3},\d{1,3}\)""".r
+        val parsed = parseLine.findAllIn(line).toList
+        logger.debug(s"parsed: ${parsed}")
+
+        val parseInstruction: matching.Regex = """(\w+)\s*\((\d+),\s*(\d+)\)""".r
+        parsed.map { instruction => instruction match {
+          case parseInstruction("mul", op1, op2) => Mul(op1.toInt, op2.toInt)
+          case _ => throw new RuntimeException("Unexpected case")
+        }}
+      }}.flatten
     } finally {
       source.close()
     }
--- test/src/aoc2024/Day03Test.scala
+++ test/src/aoc2024/Day03Test.scala
@@ -1,20 +1,24 @@
 package aoc2024
 
 class Day03Test extends munit.ScalaCheckSuite {
+  import aoc2024.Day03.Mul
+
   test("Day03 - readFile") {
     val obtained = Day03.readFile("./inputs/Day03.txt")
-    assertEquals(obtained, Seq(1, 2, 3))
+    assertEquals(obtained(0)(0), Mul(948,148))
   }
 
-  val input = Seq(0)
+  val input = Seq(
+    Seq(Mul(2, 4), Mul(5, 5), Mul(11, 8), Mul(8, 5)),
+  )
 
   test("Day03 - part1") {
     val obtained = Day03.part1(input)
-    assertEquals(obtained, 0)
+    assertEquals(obtained, 161)
   }
 
   test("Day03 - part2") {
     val obtained = Day03.part2(input)
-    assertEquals(obtained, 0)
+    assertEquals(obtained, 8)
   }
 }
