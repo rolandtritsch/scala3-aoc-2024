dfdac8f65c0a22c79d3e1ca64bc0b67caf00a763
Roland Tritsch <roland@tritsch.email>
2024-12-03 04:44:39 +0000

    Day01: Add readFile() test.

--- test/src/aoc2024/Day01Test.scala
+++ test/src/aoc2024/Day01Test.scala
@@ -1,6 +1,11 @@
 package aoc2024
 
 class Day01Test extends munit.ScalaCheckSuite {
+  test("Day01 - readFile") {
+    val obtained = Day01.readFile("./inputs/Day01.txt")
+    assertEquals(obtained(0), (64430, 75582))
+  }
+
   val input = Seq(
     (3, 4),
     (4, 3),
