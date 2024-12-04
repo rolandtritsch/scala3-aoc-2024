e5891c7203ab1ae39da420f1edc62c16bb5510fd
Roland Tritsch <roland@tritsch.email>
2024-12-04 10:04:26 +0000

    Day04: Solved.

--- src/aoc2024/Day04.scala
+++ src/aoc2024/Day04.scala
@@ -1,44 +1,175 @@
 package aoc2024
 
-/** Day04 - template
+/** Day04 - Ceres Search
+  *
+  * part1:
+  * 
+  * Just search horizontally ...
+  *
+  * - First one on the array as it is
+  * - Then on the transposed array
+  * - Then on the top-left-to-bottom-right (TLBR) array
+  * - Then on the top-right-to-bottom-left (TRBL) array
+  *
+  * Always search for the string and the reverse of the string.
+  *
+  * Sum it up.
+  *
+  * part2:
+  *
+  * - Look for MAS (and SAM) on the diagonals (TLBR and TRBL)
+  * - Get the location/co-ordinates of the As
+  * - Intersect the TLBR A locations with the TRBL A locations
+  * - Count the number of intersections
+  *
+  *  Done.
   */
 
 object Day04 {
   val logger = com.typesafe.scalalogging.Logger(this.getClass.getName)
 
-  /** @return the solution for part1 */
-  def part1(is: Seq[Int]): Int = {
-    require(is.nonEmpty, "is.nonEmpty")
-    logger.debug(s"${is}")
-
-    is(0)
-  }
-
-  /** @return the solution for part2 */
-  def part2(is: Seq[Int]): Int = {
-    require(is.nonEmpty, "is.nonEmpty")
-    logger.debug(s"${is}")
-
-    is(0)
-  }
 
   /** @return the file for the given filename as parsed elements */ 
-  def readFile(filename: String): Seq[Int] = {
+  def readFile(filename: String): Seq[String] = {
     import scala.io.Source
 
     require(filename.nonEmpty, "filename.nonEmpty")
-    logger.debug(s"${filename}")
+    logger.debug(s"filename: ${filename}")
 
     val source = Source.fromFile(filename)
     try {
       source.getLines().toSeq.map { line =>
-        logger.debug(s"${line}")
-        val parsed = line.toInt
-        logger.debug(s"${parsed}")
-        parsed
+        logger.debug(s"line: ${line}")
+        line
       }
     } finally {
       source.close()
     }
+  }
+
+  type Position = (Int, Int)
+
+  extension (line: String) {
+    def findAllPos(what: String): Seq[Int] = {
+      val parser: scala.util.matching.Regex = what.r
+      parser.findAllIn(line).matchData.map(_.start).toSeq
+    }
+  }
+
+  extension (lines: Seq[(Position, String)]) {
+    def findAllPosTLBR(what: String): Seq[Position] = {
+      lines.flatMap { case ((x, y), line) => {
+        line.findAllPos(what).map { offset => {
+          (x + offset + 1, y + offset + 1)
+        }}
+      }}
+    }
+
+    def findAllPosTRBL(what: String): Seq[Position] = {
+      lines.flatMap { case ((x, y), line) => {
+        line.findAllPos(what).map { offset => {
+          (x + offset + 1, y - offset - 1)
+        }}
+      }}
+    }
+  }
+
+  extension (lines: Seq[String]) {
+    def findAll(what: String): Int = {
+      lines.foldLeft(0) { (matchCount, line) => {
+        val parser: scala.util.matching.Regex = what.r
+        matchCount + parser.findAllIn(line).size
+      }}
+    }
+
+    def transpose0: Seq[String] = {
+      val transposed: Seq[Seq[Char]] = lines.map(_.toSeq).transpose
+      transposed.map(_.mkString)
+    }
+
+    def diagonalizeTLBR: Seq[(Position, String)] = {
+      val grid: Seq[Seq[Char]] = lines.map(_.toSeq)
+      val rows = grid.length
+      val cols = grid(0).length
+
+      def collectDiagonal(startRow: Int, startCol: Int): (Position, String) = {
+        val builder = new StringBuilder()
+        var row = startRow
+        var col = startCol
+
+        while (row < rows && col < cols) {
+          builder += grid(row)(col)
+          row += 1
+          col += 1
+        }
+
+        ((startRow, startCol), builder.result())
+      }
+
+      val diagonals =
+        (0 until cols).map(col => collectDiagonal(0, col)) ++
+          (1 until rows).map(row => collectDiagonal(row, 0))
+      diagonals
+    }
+
+    def diagonalizeTRBL: Seq[(Position, String)] = {
+      val grid: Seq[Seq[Char]] = lines.map(_.toSeq)
+      val rows = grid.length
+      val cols = grid(0).length
+
+      def collectDiagonal(startRow: Int, startCol: Int): (Position, String) = {
+        val builder = new StringBuilder()
+        var row = startRow
+        var col = startCol
+
+        while (row < rows && col >= 0) {
+          builder += grid(row)(col)
+          row += 1
+          col -= 1
+        }
+
+        ((startRow, startCol), builder.result())
+      }
+
+      val diagonals =
+        (0 until rows).map(row => collectDiagonal(row, cols - 1)) ++
+          (0 until cols - 1).map(col => collectDiagonal(0, col))
+      diagonals
+    }
+  }
+
+  /** @return the solution for part1 */
+  def part1(puzzle: Seq[String]): Int = {
+    require(puzzle.nonEmpty, "puzzle.nonEmpty")
+    logger.debug(s"puzzle: ${puzzle}")
+
+    val what = "XMAS"
+
+    puzzle.findAll(what)
+    + puzzle.findAll(what.reverse)
+    + puzzle.transpose0.findAll(what)
+    + puzzle.transpose0.findAll(what.reverse)
+    + puzzle.diagonalizeTLBR.map(_._2).findAll(what)
+    + puzzle.diagonalizeTLBR.map(_._2).findAll(what.reverse)
+    + puzzle.diagonalizeTRBL.map(_._2).findAll(what)
+    + puzzle.diagonalizeTRBL.map(_._2).findAll(what.reverse)
+  }
+
+  /** @return the solution for part2 */
+  def part2(puzzle: Seq[String]): Int = {
+    require(puzzle.nonEmpty, "puzzle.nonEmpty")
+    logger.debug(s"puzzle: ${puzzle}")
+
+    val what = "MAS"
+
+    val positionsTLBR =
+      puzzle.diagonalizeTLBR.findAllPosTLBR(what)
+      ++ puzzle.diagonalizeTLBR.findAllPosTLBR(what.reverse)
+
+    val positionsTRBL =
+      puzzle.diagonalizeTRBL.findAllPosTRBL(what)
+      ++ puzzle.diagonalizeTRBL.findAllPosTRBL(what.reverse)
+
+    positionsTRBL.intersect(positionsTLBR).size
   }
 }
--- test/src/aoc2024/Day04Test.scala
+++ test/src/aoc2024/Day04Test.scala
@@ -1,20 +1,133 @@
 package aoc2024
 
 class Day04Test extends munit.ScalaCheckSuite {
-  test("Day04 - readFile") {
-    val obtained = Day04.readFile("./inputs/Day04.txt")
-    assertEquals(obtained, Seq(1, 2, 3))
+  test("Day04 - findAll") {
+    import aoc2024.Day04._
+
+    val obtained = Seq("..XMASXMAS.XMAS", "XMAS").findAll("XMAS")
+    val expected = 4
+
+    assertEquals(obtained, expected)
   }
 
-  val input = Seq(0)
+  test("Day04 - transpose") {
+    import aoc2024.Day04._
+
+    val input = Seq("123", "ABC", "abc")
+    val obtained = input.transpose0
+    val expected = Seq("1Aa", "2Bb", "3Cc")
+
+    assertEquals(obtained, expected)
+
+    assertEquals(obtained.transpose0, input)
+  }
+
+  test("Day04 - diagonalize - tlbr") {
+    import aoc2024.Day04._
+
+    val input = Seq(
+      "1234",
+      "ABCD",
+      "abcd",
+      "+-*/",
+    )
+    val obtained = input.diagonalizeTLBR
+    val expected = Seq(
+      ((0, 0), "1Bc/"),
+      ((0, 1), "2Cd"),
+      ((0, 2), "3D"),
+      ((0, 3), "4"),
+      ((1, 0), "Ab*"),
+      ((2, 0), "a-"),
+      ((3, 0), "+"),
+    )
+
+    assertEquals(obtained, expected)
+  }
+
+  test("Day04 - diagonalize - trbl") {
+    import aoc2024.Day04._
+
+    val input = Seq(
+      "1234",
+      "ABCD",
+      "abcd",
+      "+-*/",
+    )
+    val obtained = input.diagonalizeTRBL
+    val expected = Seq(
+      ((0, 3), "4Cb+"),
+      ((1, 3), "Dc-"),
+      ((2, 3), "d*"),
+      ((3, 3), "/"),
+      ((0, 0), "1"),
+      ((0, 1), "2A"),
+      ((0, 2), "3Ba"),
+    )
+
+    assertEquals(obtained, expected)
+  }
+
+  test("Day04 - findAllPos - String") {
+    import aoc2024.Day04._
+
+    assertEquals(".MAS.MAS".findAllPos("MAS"), Seq(1,5))
+  }
+
+  test("Day04 - findAllPosTLBR - Puzzle") {
+    import aoc2024.Day04._
+
+    val input = Seq(
+      "M.S",
+      ".A.",
+      "M.S",
+    )
+    val obtained = input.diagonalizeTLBR.findAllPosTLBR("MAS")
+    val expected = Seq((1, 1))
+
+    assertEquals(obtained, expected)
+  }
+
+  test("Day04 - findAllPosTRBL - Puzzle") {
+    import aoc2024.Day04._
+
+    val input = Seq(
+      "M.S",
+      ".A.",
+      "M.S",
+    )
+    val obtained = input.diagonalizeTRBL.findAllPosTRBL("SAM")
+    val expected = Seq((1, 1))
+
+    assertEquals(obtained, expected)
+  }
+
+  test("Day04 - readFile") {
+    val obtained = Day04.readFile("./inputs/Day04.txt")
+    val expected = "XMXXMSSSMSXSXMMXSAMMXXSXMASMSSXXMAMXAMXSXMXSMAMMASXXASMMXMASXMSSXMMMXMXSXXSXMXXSAMXSXSXSAMXMSAMXMAXXXMXMAMSASXMSSXMSXSXXMAXXSSSMXMXMXMMAASXM"
+    assertEquals(obtained(0), expected)
+  }
+
+  val input = Seq(
+    "MMMSXXMASM",
+    "MSAMXMSMSA",
+    "AMXSXMAAMM",
+    "MSAMASMSMX",
+    "XMASAMXAMM",
+    "XXAMMXXAMA",
+    "SMSMSASXSS",
+    "SAXAMASAAA",
+    "MAMMMXMMMM",
+    "MXMXAXMASX",
+  )
 
   test("Day04 - part1") {
     val obtained = Day04.part1(input)
-    assertEquals(obtained, 0)
+    assertEquals(obtained, 18)
   }
 
   test("Day04 - part2") {
     val obtained = Day04.part2(input)
-    assertEquals(obtained, 0)
+    assertEquals(obtained, 9)
   }
 }
