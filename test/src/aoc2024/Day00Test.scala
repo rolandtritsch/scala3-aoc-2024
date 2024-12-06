40c11617da5945c1705d838bc37d0a8ffb010425
Roland Tritsch <roland@tritsch.email>
2024-12-06 17:42:12 +0000

    Day00: Add ignore tag to template.

--- test/src/aoc2024/Day00Test.scala
+++ test/src/aoc2024/Day00Test.scala
@@ -1,6 +1,12 @@
 package aoc2024
 
 class Day00Test extends munit.ScalaCheckSuite {
+  val ignore = new munit.Tag("ignore")
+
+  test("Day00 - dummy".tag(ignore)) {
+    assert(true)
+  }
+
   test("Day00 - readFile") {
     val obtained = Day00.readFile("./inputs/Day00.txt")
     assertEquals(obtained, Seq(1, 2, 3))
