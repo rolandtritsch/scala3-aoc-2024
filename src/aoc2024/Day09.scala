e079c4138d786c64cd1c912a7f5ec1d6ef2fdb11
Roland Tritsch <roland@tritsch.email>
2024-12-10 05:05:51 +0000

    Day09: part2: Solved. Needs refactoring.

--- src/aoc2024/Day09.scala
+++ src/aoc2024/Day09.scala
@@ -74,7 +74,6 @@ object Day09 {
     }
   }
 
-
   /** @return the file for the given filename as parsed elements */ 
   def readFile(filename: String): Disk = {
     import scala.io.Source
@@ -102,6 +101,147 @@ object Day09 {
     }
   }
 
+  import scala.collection.mutable
+
+  type Block2 = (Int, Int, Option[Int])
+
+  /** The disk */
+  class Disk2 (val blocks: mutable.ListBuffer[Block2]) {
+    override def toString(): String = {
+      blocks.map { (_, blockSize, id) => id match {
+        case None => '.'.toString * blockSize
+        case Some(id) => id.toString * blockSize
+      }}.mkString
+    }
+
+    def fileSystemCheck: Boolean = {
+      val firstBlock = blocks.head
+      blocks.tail.foldLeft(firstBlock) { case(pb, b) => {
+        logger.debug(s"pb: ${pb}, b: ${b}")
+
+        val (poffset, psize, pid) = pb
+        val (offset, _, id) = b
+
+        assert(poffset + psize == offset)
+        assert(pid.getOrElse(-99) != id.getOrElse(-99))
+        b
+      }}
+      true
+    }
+
+    /** map to look up the size of the file by file id */
+    val fileSizes = blocks.filter(_._3.isDefined).map((_, size, id) => (id.get, size)).toMap
+
+    /** @return the index of the file */
+    def findFileIndex(id0: Int): Int = {
+      val index = blocks.indexWhere { (_, _, id1) => id1 match {
+        case Some(id2) if(id2 == id0) => true
+        case _ => false
+      }}
+      assert(index >= 0, "file id not found")
+      index
+    }
+
+    /** @return the index of the first free space of minSize size */
+    def findFirstFreeSpaceIndex(minSize: Int): Int = {
+      blocks.indexWhere { (_, size, id) => id match {
+        case None if(size >= minSize) => true
+        case _ => false
+      }}
+    }
+
+    /** update the block list with the free space rightsized to be swapped */
+    def split(freeIndex: Int, minSize: Int): Unit = {
+      val block = blocks(freeIndex)
+      val (offset, size, id) = block
+      val splitBlocks =
+        if(size > minSize) List((offset, minSize, id), (offset + minSize, size - minSize, id))
+        else List(block)
+
+      blocks.remove(freeIndex)
+      blocks.insertAll(freeIndex, splitBlocks)
+    }
+
+    /** update the block list with the swapped blocks */
+    def swap(freeIndex: Int, fileIndex: Int): Unit = {
+      val (fileOffset, fileSize, fileId) = blocks.remove(fileIndex)
+      val (freeOffset, freeSize, freeId) = blocks.remove(freeIndex)
+  
+      blocks.insert(freeIndex, (freeOffset, fileSize, fileId))
+      blocks.insert(fileIndex, (fileOffset, freeSize, freeId))
+      }
+
+    /** merge the two block, if they are both free space */
+    def merge(index0: Int, index1: Int): Unit = {
+      if (index0 >= 0 && index1 < blocks.size) {
+        val (offset0, size0, id0) = blocks(index0)
+        val (offset1, size1, id1) = blocks(index1)
+        if (!id0.isDefined && !id1.isDefined) {
+          blocks.remove(index1)
+          blocks.remove(index0)
+          blocks.insert(index0, (offset0, size0 + size1, id0))
+        }
+      }
+    }
+
+    /** defragment the file on the disk */
+    def defragment(id: Int): Unit = {
+      val freeIndex = findFirstFreeSpaceIndex(fileSizes(id))
+      if (freeIndex >= 0) {
+        split(freeIndex, fileSizes(id))
+        val fileIndex = findFileIndex(id)
+        if (freeIndex < fileIndex) {
+          swap(freeIndex, fileIndex)
+          merge(fileIndex, fileIndex + 1)
+          merge(fileIndex - 1, fileIndex)
+        }
+      }
+    }
+
+    def maxId: Int = {
+      val (_, _, Some(id)) = blocks.last: @unchecked
+      id
+    }
+
+    def checksum: BigInt = {
+      val ids = blocks.flatMap((_, size, id) => List.fill(size)(BigInt(id.getOrElse(0))))
+      ids.zipWithIndex.map(_ * _).sum
+    }
+  }
+
+  /** @return the file for the given filename as parsed elements */ 
+  def readFile2(filename: String): Disk2 = {
+    import scala.io.Source
+
+    require(filename.nonEmpty, "filename.nonEmpty")
+    logger.debug(s"filename: ${filename}")
+
+    val emptyBlocks = mutable.ListBuffer[Block2]()
+    val source = Source.fromFile(filename)
+    try {
+      val line = source.getLines.toSeq.head
+      logger.debug(s"line: ${line}")
+
+      val firstBlockSize = line(0).toString.toInt
+      val firstBlock = emptyBlocks :+ (0, firstBlockSize, Some(0))
+
+      val (blocks, _, _) = line.tail.grouped(2).foldLeft(firstBlock, firstBlockSize, 1) { case ((bs, offset, id), chars) => {
+        logger.debug(s"bs: ${bs}, chars: ${chars}, id: ${id}}")
+
+        val freeBlockSize = chars(0).toString.toInt
+        val freeBlocks = (offset, freeBlockSize, None)
+        val usedBlockSize = chars(1).toString.toInt
+        val usedBlocks = (offset + freeBlockSize, usedBlockSize, Some(id))
+
+        (bs :+ freeBlocks :+ usedBlocks, offset + freeBlockSize + usedBlockSize, id + 1)
+      }}
+
+      Disk2(blocks)
+    } finally {
+      source.close()
+    }
+  }
+
   /** @return the checksum for the defragmented disk */
   def part1(disk: Disk): BigInt = {
     require(disk.blocks.nonEmpty, "disk.blocks.nonEmpty")
@@ -110,11 +250,13 @@ object Day09 {
     disk.defragment.checksum
   }
 
-  /** @return the solution for part2 */
-  def part2(disk: Disk): BigInt = {
+  /** @return the checksum for the defragmented disk */
+  def part2(disk: Disk2): BigInt = {
     require(disk.blocks.nonEmpty, "disk.blocks.nonEmpty")
     logger.debug(s"disk: ${disk}")
 
-    BigInt(0)
+    (disk.maxId to 1 by -1).foreach(disk.defragment(_))
+    //disk.fileSystemCheck
+    disk.checksum
   }
 }
--- test/src/aoc2024/Day09Test.scala
+++ test/src/aoc2024/Day09Test.scala
@@ -19,12 +19,80 @@ class Day09Test extends munit.ScalaCheckSuite {
     assertEquals(obtained.blocks.size, 95482)
   }
 
+  test("Day09 - readFile2 - test") {
+    val obtained = Day09.readFile2("./inputs/Day09Test.txt")
+    assertEquals(obtained.blocks.size, 19)
+    assertEquals(obtained.toString, "00...111...2...333.44.5555.6666.777.888899")
+    assert(obtained.fileSystemCheck)
+  }
+
   test("Day09 - defragment") {
     val disk = Day09.readFile("./inputs/Day09Test.txt")
     val obtained = disk.defragment
     val expected = "0099811188827773336446555566.............."
 
     assertEquals(obtained.toString, expected)
+  }
+
+  test("Day09 - findFileIndex") {
+    val disk = Day09.readFile2("./inputs/Day09Test.txt")
+
+    assertEquals(disk.findFileIndex(9), 18)
+    assertEquals(disk.findFileIndex(0), 0)
+    assertEquals(disk.findFileIndex(2), 4)
+  }
+
+  test("Day09 - findFileSizes") {
+    val disk = Day09.readFile2("./inputs/Day09Test.txt")
+
+    assertEquals(disk.fileSizes(9), 2)
+    assertEquals(disk.fileSizes(2), 1)
+    assertEquals(disk.fileSizes(7), 3)
+  }
+
+  test("Day09 - findFirstFreeSpaceIndex") {
+    val disk = Day09.readFile2("./inputs/Day09Test.txt")
+
+    assertEquals(disk.findFirstFreeSpaceIndex(1), 1)
+    assertEquals(disk.findFirstFreeSpaceIndex(2), 1)
+  }
+
+  test("Day09 - split") {
+    val disk = Day09.readFile2("./inputs/Day09Test.txt")
+    disk.split(1, 2)
+    val expected = "00...111...2...333.44.5555.6666.777.888899"
+
+    assertEquals(disk.toString, expected)
+  }
+
+  test("Day09 - swap") {
+    val disk = Day09.readFile2("./inputs/Day09Test.txt")
+    disk.split(1, 2)
+    assertEquals(disk.toString, "00...111...2...333.44.5555.6666.777.888899")
+    disk.swap(1, 19)
+    assertEquals(disk.toString, "0099.111...2...333.44.5555.6666.777.8888..")
+  }
+
+  test("Day09 - defragment2") {
+    val disk = Day09.readFile2("./inputs/Day09Test.txt")
+    disk.defragment(9)
+    assertEquals(disk.toString, "0099.111...2...333.44.5555.6666.777.8888..")
+    disk.defragment(8)
+    assertEquals(disk.toString, "0099.111...2...333.44.5555.6666.777.8888..")
+    disk.defragment(7)
+    assertEquals(disk.toString, "0099.1117772...333.44.5555.6666.....8888..")
+    disk.defragment(6)
+    assertEquals(disk.toString, "0099.1117772...333.44.5555.6666.....8888..")
+    disk.defragment(5)
+    assertEquals(disk.toString, "0099.1117772...333.44.5555.6666.....8888..")
+    disk.defragment(4)
+    assertEquals(disk.toString, "0099.111777244.333....5555.6666.....8888..")
+    disk.defragment(3)
+    assertEquals(disk.toString, "0099.111777244.333....5555.6666.....8888..")
+    disk.defragment(2)
+    assertEquals(disk.toString, "00992111777.44.333....5555.6666.....8888..")
+    disk.defragment(1)
+    assertEquals(disk.toString, "00992111777.44.333....5555.6666.....8888..")
   }
 
   test("Day09 - part1 - test") {
@@ -39,16 +107,15 @@ class Day09Test extends munit.ScalaCheckSuite {
     assertEquals(obtained, BigInt("6432869891895"))
   }
 
-  test("Day09 - part2 - test") {
-    val input = Day09.readFile("./inputs/Day09Test.txt")
+  test("Day09 - part2 - test".tag(only)) {
+    val input = Day09.readFile2("./inputs/Day09Test.txt")
     val obtained = Day09.part2(input)
-    //assertEquals(obtained, BigInt("2858"))
-    assertEquals(obtained, BigInt("0"))
+    assertEquals(obtained, BigInt("2858"))
   }
 
   test("Day09 - part2") {
-    val input = Day09.readFile("./inputs/Day09.txt")
+    val input = Day09.readFile2("./inputs/Day09.txt")
     val obtained = Day09.part2(input)
-    assertEquals(obtained, BigInt("0"))
+    assertEquals(obtained, BigInt("6467290479134"))
   }
 }
