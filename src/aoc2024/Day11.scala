8f7f965f5a4c92e203e23be9d4de222c174e2331
Roland Tritsch <roland@tritsch.email>
2024-12-11 07:44:06 +0000

    Day11: part1: Solved.

--- src/aoc2024/Day11.scala
+++ src/aoc2024/Day11.scala
@@ -1,13 +1,32 @@
 package aoc2024
 
-/** Day11 - template
+/** Day11 - Plutonian Pebbles
+  *
+  * That sounds easy enough.
+  * 
+  * Things to think about ...
+  *
+  * - for every step we need to traverse the entire list
+  * - to support the splitting of the stones I was (initially) thinking
+  *   to use a double-linked list again, but (because) we traverse (and
+  *   modify) the list (and do not need index access to the list elements) 
+  *   we probably can (just) use a List
+  * - lets assume that the numbers in the list can get big and use a list
+  *   of BigInt
+  * - to walk the list we can use a loop, a map or a recursion. I will start
+  *   with a map and see how it goes
+  *
+  * part1:
+  *
+  * - run over the list N times and apply the rules to each element
+  * - done
   */
 
 object Day11 {
   val logger = com.typesafe.scalalogging.Logger(this.getClass.getName)
 
-  /** @return the file for the given filename as parsed elements */ 
-  def readFile(filename: String): Seq[Int] = {
+  /** @return parse the given file and return the list of numbers */
+  def readFile(filename: String): List[BigInt] = {
     import scala.io.Source
 
     require(filename.nonEmpty, "filename.nonEmpty")
@@ -15,30 +34,65 @@ object Day11 {
 
     val source = Source.fromFile(filename)
     try {
-      source.getLines().toSeq.map { line =>
-        logger.debug(s"line: ${line}")
-        val parsed = line.toInt
-        logger.debug(s"parsed: ${parsed}")
-        parsed
-      }
+      source.getLines.next.stones
     } finally {
       source.close()
     }
   }
 
-  /** @return the solution for part1 */
-  def part1(is: Seq[Int]): Int = {
-    require(is.nonEmpty, "is.nonEmpty")
-    logger.debug(s"is: ${is}")
+  type Stone = BigInt
+  type Rule = Stone => (List[Stone], Boolean)
 
-    is(0)
+  private def ruleZero(stone: Stone): (List[Stone], Boolean) = {
+    if(stone == 0) (List(BigInt(1)), true) else (List(BigInt(0)), false)  
+  }
+
+  private def ruleEven(stone: Stone): (List[Stone], Boolean) = {
+    val stoneString = stone.toString
+    if(stoneString.length % 2 == 0) {
+      val (first, second) = stoneString.splitAt(stoneString.length / 2)  
+      (List(BigInt(first), BigInt(second)), true)
+    } else 
+      (List(stone), false)  
+  }
+
+  private def ruleDefault(stone: Stone): (List[Stone], Boolean) = {
+    (List(stone * 2024), true)
+  }
+
+  val rules = List(ruleZero, ruleEven, ruleDefault)
+
+  extension (stones: List[BigInt]) {
+    def apply(rules: List[Rule]): List[BigInt] = {
+      stones.flatMap { stone => 
+        rules.foldLeft(List[BigInt](), false) { case ((newStones, done), rule) => 
+          if(!done) rule(stone) else (newStones, done)
+        }._1
+      }
+    }
+
+    def applyN(rules: List[Rule], n: Int): List[BigInt] = {
+      if (n == 0) stones else stones.apply(rules).applyN(rules, n - 1)
+    }
+  }
+
+  extension (s: String) {
+    def stones: List[BigInt] = s.split(" ").toList.map(BigInt(_))
+  }
+
+  /** @return the number of stones on the list */
+  def part1(stones: List[BigInt]): BigInt = {
+    require(stones.nonEmpty, "stones.nonEmpty")
+    logger.debug(s"stones: ${stones}")
+
+    stones.applyN(rules, 25).size
   }
 
   /** @return the solution for part2 */
-  def part2(is: Seq[Int]): Int = {
-    require(is.nonEmpty, "is.nonEmpty")
-    logger.debug(s"is: ${is}")
+  def part2(stones: List[BigInt]): BigInt = {
+    require(stones.nonEmpty, "stones.nonEmpty")
+    logger.debug(s"stones: ${stones}")
 
-    is(0)
+    stones(0)
   }
 }
--- test/src/aoc2024/Day11Test.scala
+++ test/src/aoc2024/Day11Test.scala
@@ -1,5 +1,7 @@
 package aoc2024
 
+import aoc2024.Day11.stones
+
 class Day11Test extends munit.ScalaCheckSuite {
   val only = new munit.Tag("only")
   val ignore = new munit.Tag("ignore")
@@ -8,20 +10,53 @@ class Day11Test extends munit.ScalaCheckSuite {
     assert(true)
   }
 
-  test("Day11 - readFile") {
-    val obtained = Day11.readFile("./inputs/Day11.txt")
-    assertEquals(obtained, Seq(1, 2, 3))
+  test("Day11 - readFile - test") {
+    val obtained = Day11.readFile("./inputs/Day11Test.txt")
+    assertEquals(obtained.head, BigInt(125))
   }
 
-  val input = Seq(0)
+  test("Day11 - readFile") {
+    val obtained = Day11.readFile("./inputs/Day11.txt")
+    assertEquals(obtained.head, BigInt(475449))
+  }
+
+  test("Day11 - apply") {
+    val input = "1036288 7 2 20 24 4048 1 4048 8096 28 67 60 32".stones
+    val obtained = input.apply(Day11.rules)
+    val expexted = "2097446912 14168 4048 2 0 2 4 40 48 2024 40 48 80 96 2 8 6 7 6 0 3 2".stones
+
+    assertEquals(obtained, expexted)
+  }
+
+  test("Day11 - apply") {
+    val input = "0 1 10 99 999".stones
+    val obtained = input.apply(Day11.rules)
+    val expexted = "1 2024 1 0 9 9 2021976".stones
+
+    assertEquals(obtained, expexted)
+  }
+
+  test("Day11 - part1 - test") {
+    val input = Day11.readFile("./inputs/Day11Test.txt")
+    val obtained = Day11.part1(input)
+    assertEquals(obtained, BigInt(55312))
+  }
 
   test("Day11 - part1") {
+    val input = Day11.readFile("./inputs/Day11.txt")
     val obtained = Day11.part1(input)
-    assertEquals(obtained, 0)
+    assertEquals(obtained, BigInt(193269))
+  }
+
+  test("Day11 - part2 - test") {
+    val input = Day11.readFile("./inputs/Day11Test.txt")
+    val obtained = Day11.part2(input)
+    assertEquals(obtained, BigInt(125))
   }
 
   test("Day11 - part2") {
+    val input = Day11.readFile("./inputs/Day11.txt")
     val obtained = Day11.part2(input)
-    assertEquals(obtained, 0)
+    assertEquals(obtained, BigInt(475449))
   }
 }
