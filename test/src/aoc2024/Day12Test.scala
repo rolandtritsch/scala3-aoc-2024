2cdc6d094f45e3d239ce60c98ec8c9bf771aac54
Roland Tritsch <roland@tritsch.email>
2024-12-12 19:13:35 +0000

    Day12: Updated tests.

--- test/src/aoc2024/Day12Test.scala
+++ test/src/aoc2024/Day12Test.scala
@@ -8,19 +8,36 @@ class Day12Test extends munit.ScalaCheckSuite {
     assert(true)
   }
 
-  test("Day12 - readFile") {
-    val obtained = Day12.readFile("./inputs/Day12.txt")
-    assertEquals(obtained, Seq(1, 2, 3))
+  test("Day12 - readFile - test") {
+    val obtained = Day12.readFile("./inputs/Day12Test.txt")
+    assertEquals(obtained.regions.size, 100)
   }
 
-  val input = Seq(0)
+  test("Day12 - readFile") {
+    val obtained = Day12.readFile("./inputs/Day12.txt")
+    assertEquals(obtained.regions.size, 19600)
+  }
 
-  test("Day12 - part1") {
+  test("Day12 - part1 - test".tag(only)) {
+    val input = Day12.readFile("./inputs/Day12Test3.txt")
     val obtained = Day12.part1(input)
     assertEquals(obtained, 0)
   }
 
+  test("Day12 - part1") {
+    val input = Day12.readFile("./inputs/Day12.txt")
+    val obtained = Day12.part1(input)
+    assertEquals(obtained, 0)
+  }
+
+  test("Day12 - part2 - test") {
+    val input = Day12.readFile("./inputs/Day12Test.txt")
+    val obtained = Day12.part2(input)
+    assertEquals(obtained, 0)
+  }
+
   test("Day12 - part2") {
+    val input = Day12.readFile("./inputs/Day12.txt")
     val obtained = Day12.part2(input)
     assertEquals(obtained, 0)
   }
