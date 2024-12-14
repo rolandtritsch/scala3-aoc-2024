5817cd7ee388c2e21a3cbac2c4e4205d796b8987
Roland Tritsch <roland@tritsch.email>
2024-12-14 17:58:49 +0000

    Day14: part1: Solved.

--- src/aoc2024/Day14.scala
+++ src/aoc2024/Day14.scala
@@ -1,44 +1,164 @@
 package aoc2024
 
-/** Day14 - template
+/** Day14 - Restroom Redoubt
+  *
+  * @see https://adventofcode.com/2024/day/14
+  *
+  * OK ... we have Robots and Positions and Velocities.
+  * We can go with a 2-dimensional array or with case
+  * classes. I think I'll go with case classes.
+  *
+  * part1:
+  *
+  * - Read the input file
+  * - Move the robots around for 100 seconds
+  * - Get the quadrants and count the number of robots in each
+  * - Multiply the number of robots in each quadrant
+  *
+  * part2:
+  *
+  * Not sure yet. Not clear how the christmas tree should look like.
+  * After I find out how the christmas tree should look like, we can
+  * run the simulation one step at a time to find out of if the result
+  * matches the shape of the christmas tree. 
   */
 
 object Day14 {
   val logger = com.typesafe.scalalogging.Logger(this.getClass.getName)
 
+  /** A position in the 2-dimensional space */
+  case class Position(x: Int, y: Int) {
+    private[Day14] def add(v: Velocity, dimensions: (Int, Int)): Position = {
+      val (maxX, maxY) = dimensions
+      val newX =
+        if(this.x + v.dx >= maxX) this.x + v.dx - maxX
+        else if(this.x + v.dx < 0) this.x + v.dx + maxX
+        else this.x + v.dx
+      val newY =
+        if(this.y + v.dy >= maxY) this.y + v.dy - maxY
+        else if(this.y + v.dy < 0) this.y + v.dy + maxY
+        else this.y + v.dy
+      Position(newX, newY)
+    }
+  }
+
+  object Position {
+    implicit val ordering: Ordering[Position] = Ordering.by(p => (p.x, p.y))
+  }
+  import scala.math.Ordering.Implicits._
+
+  /** A velocity in the 2-dimensional space */
+  case class Velocity(dx: Int, dy: Int)
+
+  /** A robot with an id, a position and a velocity */
+  case class Robot(id: Int, position: Position, velocity: Velocity)
+
+  extension (robots: Set[Robot]) {
+    def move(dimensions: (Int, Int)): Set[Robot] = {
+      robots.map { robot =>
+        val newPosition = robot.position.add(robot.velocity, dimensions)
+        Robot(robot.id, newPosition, robot.velocity)
+      }
+    }
+  }
+
+  /** A simulation with a set of robots and dimensions */
+  class Simulation(robots: Set[Robot], dimensions: (Int, Int)) {
+    /** @return a new simulation after running for the given seconds */
+    def run(seconds: Int): Simulation = {
+      val newRobots = (1 to seconds).foldLeft(robots) { (rs, _) =>
+        rs.move(dimensions)
+      }
+
+      Simulation(newRobots, dimensions)
+    }
+
+    /** @return the number of robots in each quadrant */
+    def quadrants(): Map[(Int, Int), Int] = {
+      def isValid(robot: Robot): Boolean = {
+        val (midX, midY) = (dimensions._1 / 2, dimensions._2 / 2)
+        robot.position.x != midX && robot.position.y != midY
+      }
+
+      def byQuadrant(robot: Robot): (Int, Int) = {
+        val (midX, midY) = (dimensions._1 / 2, dimensions._2 / 2)
+        val qX = if(robot.position.x < midX) 0 else 1
+        val qY = if(robot.position.y < midY) 0 else 1
+        (qX, qY)
+      }
+
+      robots.filter(isValid).groupBy(byQuadrant).map { case (k, v) =>
+        (k, v.size)
+      }
+    }
+
+    override def toString(): String = {
+      val (maxX, maxY) = dimensions
+      val rs =
+        robots.toList.sortBy(_.position).map { r =>
+          s"Robot(${r.id}, ${r.position}, ${r.velocity})"
+        }.mkString("\n")
+      s"Simulation(${robots.size}/${dimensions}):\n${rs}\n"
+    }
+
+    def toStringPretty(): String = {
+      val (maxX, maxY) = dimensions
+      val rs =
+        (0 until maxX).map { x =>
+          (0 until maxY).map { y =>
+            robots.count(_.position == Position(x, y))
+          }.mkString("")
+        }.mkString("\n")
+      s"Simulation(${robots.size}/${dimensions}):\n${rs}\n"
+    }
+  }
+
   /** @return the file for the given filename as parsed elements */ 
-  def readFile(filename: String): Seq[Int] = {
+  def readFile(filename: String): Simulation = {
     import scala.io.Source
+    import scala.util.matching
 
     require(filename.nonEmpty, "filename.nonEmpty")
     logger.debug(s"filename: ${filename}")
 
+    var dimensions = (0, 0)
     val source = Source.fromFile(filename)
     try {
-      source.getLines().toSeq.map { line =>
+      val robots = source.getLines().toSeq.zipWithIndex.map { (line, robot) =>
         logger.debug(s"line: ${line}")
-        val parsed = line.toInt
-        logger.debug(s"parsed: ${parsed}")
-        parsed
-      }
+
+        // p=0,4 v=3,-3
+        val parser: matching.Regex = """p=(-?\d+),(-?\d+) v=(-?\d+),(-?\d+)""".r
+        val parsed = parser.findAllIn(line).matchData.next.subgroups.map(_.toInt)
+        assert(parsed.size == 4, s"parsed.size == 4: ${parsed.size}")
+
+        val (py, px, vy, vx) = (parsed(0), parsed(1), parsed(2), parsed(3))
+
+        val (maxX, maxY) = dimensions
+        dimensions = (math.max(maxX, px + 1), math.max(maxY, py + 1))
+
+        Robot(robot, Position(px, py), Velocity(vx, vy))
+      }.toSet
+
+      Simulation(robots, dimensions)
     } finally {
       source.close()
     }
   }
 
-  /** @return the solution for part1 */
-  def part1(is: Seq[Int]): Int = {
-    require(is.nonEmpty, "is.nonEmpty")
-    logger.debug(s"is: ${is}")
+  /** @return the safety factor */
+  def part1(simulation: Simulation): Int = {
+    //require(simulation.robots.nonEmpty, "simulation.robots.nonEmpty")
+    logger.debug(s"simulation: ${simulation}")
 
-    is(0)
+    simulation.run(100).quadrants().values.product
   }
 
   /** @return the solution for part2 */
-  def part2(is: Seq[Int]): Int = {
-    require(is.nonEmpty, "is.nonEmpty")
-    logger.debug(s"is: ${is}")
+  def part2(simulation: Simulation): Int = {
+    //require(simulation.robots.nonEmpty, "simulation.robots.nonEmpty")
+    logger.debug(s"simulation: ${simulation}")
 
-    is(0)
+    simulation.run(100).quadrants().values.product
   }
 }
--- test/src/aoc2024/Day14Test.scala
+++ test/src/aoc2024/Day14Test.scala
@@ -10,35 +10,42 @@ class Day14Test extends munit.ScalaCheckSuite {
 
   test("Day14 - readFile - test") {
     val obtained = Day14.readFile("./inputs/Day14Test.txt")
-    assertEquals(obtained, Seq(1, 2, 3))
+    assert(obtained.toString.nonEmpty)
   }
 
   test("Day14 - readFile") {
     val obtained = Day14.readFile("./inputs/Day14.txt")
-    assertEquals(obtained, Seq(1, 2, 3))
+    assert(obtained.toString.nonEmpty)
+  }
+
+  test("Day14 - quadrants - test") {
+    val simulation = Day14.readFile("./inputs/Day14Test.txt")
+    val obtained = simulation.run(100).quadrants().values.toList.sorted
+    val expected = List(1, 1, 3, 4)
+    assertEquals(obtained, expected)
   }
 
   test("Day14 - part1 - test") {
-    val input = Day14.readFile("./inputs/Day14.txt")
+    val input = Day14.readFile("./inputs/Day14Test.txt")
     val obtained = Day14.part1(input)
-    assertEquals(obtained, 1)
+    assertEquals(obtained, 12)
   }
 
   test("Day14 - part1") {
     val input = Day14.readFile("./inputs/Day14.txt")
     val obtained = Day14.part1(input)
-    assertEquals(obtained, 1)
+    assertEquals(obtained, 228457125)
   }
 
   test("Day14 - part2 - test") {
-    val input = Day14.readFile("./inputs/Day14.txt")
+    val input = Day14.readFile("./inputs/Day14Test.txt")
     val obtained = Day14.part2(input)
-    assertEquals(obtained, 1)
+    assertEquals(obtained, 12)
   }
 
   test("Day14 - part2") {
     val input = Day14.readFile("./inputs/Day14.txt")
     val obtained = Day14.part2(input)
-    assertEquals(obtained, 1)
+    assertEquals(obtained, 228457125)
   }
 }
