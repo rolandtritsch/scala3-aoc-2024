d8496342feb804754d6d2174edbc82287447e848
Roland Tritsch <roland@tritsch.email>
2024-12-15 07:12:43 +0000

    Day00: Update test template.

--- test/src/aoc2024/Day00Test.scala
+++ test/src/aoc2024/Day00Test.scala
@@ -1,5 +1,7 @@
 package aoc2024
 
+import Day00._
+
 class Day00Test extends munit.ScalaCheckSuite {
   val only = new munit.Tag("only")
   val ignore = new munit.Tag("ignore")
@@ -9,36 +11,36 @@ class Day00Test extends munit.ScalaCheckSuite {
   }
 
   test("Day00 - readFile - test") {
-    val obtained = Day00.readFile("./inputs/Day00Test.txt")
+    val obtained = readFile("./inputs/Day00Test.txt")
     assertEquals(obtained, Seq(1, 2, 3))
   }
 
   test("Day00 - readFile") {
-    val obtained = Day00.readFile("./inputs/Day00.txt")
+    val obtained = readFile("./inputs/Day00.txt")
     assertEquals(obtained, Seq(1, 2, 3))
   }
 
   test("Day00 - part1 - test") {
-    val input = Day00.readFile("./inputs/Day00Test.txt")
-    val obtained = Day00.part1(input)
+    val input = readFile("./inputs/Day00Test.txt")
+    val obtained = part1(input)
     assertEquals(obtained, 1)
   }
 
   test("Day00 - part1") {
-    val input = Day00.readFile("./inputs/Day00.txt")
-    val obtained = Day00.part1(input)
+    val input = readFile("./inputs/Day00.txt")
+    val obtained = part1(input)
     assertEquals(obtained, 1)
   }
 
   test("Day00 - part2 - test") {
-    val input = Day00.readFile("./inputs/Day00Test.txt")
-    val obtained = Day00.part2(input)
+    val input = readFile("./inputs/Day00Test.txt")
+    val obtained = part2(input)
     assertEquals(obtained, 1)
   }
 
   test("Day00 - part2") {
-    val input = Day00.readFile("./inputs/Day00.txt")
-    val obtained = Day00.part2(input)
+    val input = readFile("./inputs/Day00.txt")
+    val obtained = part2(input)
     assertEquals(obtained, 1)
   }
 }
