5a003f19c8fec4520ee523052b54642ddf12767d
Roland Tritsch <roland@tritsch.email>
2024-12-19 15:03:36 +0000

    Day19: Part1: Solved.

--- src/aoc2024/Day19.scala
+++ src/aoc2024/Day19.scala
@@ -1,13 +1,30 @@
 package aoc2024
 
 /** Day19 - Linen Layout
+  *
+  * Seems that (at least for part1) we can cheat a bit and use regular
+  * expressions to do the job for us.
+  * 
+  * The first line of the inpu gives us a list of tokens. We can then
+  * pattern match on the given token strings to find the ones that
+  * are possible.
+  * 
+  * Part1:
+  * 
+  * - Read the input file
+  * - Build the regular expression from the tokens
+  * - Match the token strings
+  * - Count the matches
   */
 
 object Day19 {
   val logger = com.typesafe.scalalogging.Logger(this.getClass.getName)
 
-  /** @return the file for the given filename as parsed elements */ 
-  def readFile(filename: String): Seq[Int] = {
+  type Towel = String
+  type Design = String
+
+  /** @return the set of tokens */ 
+  def readFile(filename: String): (Set[Towel], Set[Design]) = {
     import scala.io.Source
 
     require(filename.nonEmpty, "filename.nonEmpty")
@@ -15,30 +32,33 @@ object Day19 {
 
     val source = Source.fromFile(filename)
     try {
-      source.getLines().toSeq.map { line =>
-        logger.debug(s"line: ${line}")
-        val parsed = line.toInt
-        logger.debug(s"parsed: ${parsed}")
-        parsed
-      }
+      val lines = source.getLines().toSeq
+      val towels = lines.head.split(",").map(_.trim).toSet
+      val designs = lines.tail.filter(!_.isEmpty).map(_.trim).toSet
+      (towels, designs)
     } finally {
       source.close()
     }
   }
 
-  /** @return the solution for part1 */
-  def part1(is: Seq[Int]): Int = {
-    require(is.nonEmpty, "is.nonEmpty")
-    logger.debug(s"is: ${is}")
+  /** @return the number of possible towels */
+  def part1(onsen: (Set[Towel], Set[Design])): Int = {
+    require(onsen._1.nonEmpty, "onsen._1.nonEmpty")
+    require(onsen._2.nonEmpty, "onsen._2.nonEmpty")
+    logger.debug(s"onsen: ${onsen}")
 
-    is(0)
+    val (towels, designs) = onsen    
+    val parser = raw"(${towels.mkString("|")})+".r
+    designs.count(parser.matches)
   }
 
   /** @return the solution for part2 */
-  def part2(is: Seq[Int]): Int = {
-    require(is.nonEmpty, "is.nonEmpty")
-    logger.debug(s"is: ${is}")
+  def part2(onsen: (Set[Towel], Set[Design])): Int = {
+    require(onsen._1.nonEmpty, "onsen._1.nonEmpty")
+    require(onsen._2.nonEmpty, "onsen._2.nonEmpty")
+    logger.debug(s"onsen: ${onsen}")
 
-    is(0)
+    val (towels, designs) = onsen
+    designs.size
   }
 }
