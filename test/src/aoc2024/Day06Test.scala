88c9d2b2538ab3e0d29fbb57cf922b85b7a9da74
Roland Tritsch <roland@tritsch.email>
2024-12-21 07:47:22 +0000

    Day06: Setting test timeout to make CI/CD pass again.

--- test/src/aoc2024/Day06Test.scala
+++ test/src/aoc2024/Day06Test.scala
@@ -1,6 +1,8 @@
 package aoc2024
 
 class Day06Test extends munit.ScalaCheckSuite {
+  override val munitTimeout = scala.concurrent.duration.Duration(60, "s")
+
   test("Day06 - readFile - test") {
     val obtained = Day06.readFile("./inputs/Day06Test.txt")
     assertEquals(obtained.guard, Day06.Position(6, 4))
